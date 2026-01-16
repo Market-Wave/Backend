43fc1468036d2888a54b92b7e4a18f682fc331b1
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2026-01-16 09:43:42 +0530

    Initial commit

---
+++ advertisement-service/src/test/java/org/motionstack/marketwave/advertisementservice/AdvertisementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.motionstack.marketwave.advertisementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
